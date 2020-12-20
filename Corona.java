72d6ed15906dd72c1ace6d11e8561afd6097d07e
kalomoiraa <73114465+kalomoiraa@users.noreply.github.com>
2020-12-20 17:21:28 +0200

    Add files via upload

--- Corona.java
+++ Corona.java
@@ -26,20 +26,22 @@ public class Corona{
 			for (int i = 0; i < ccnum; i++) {
 				addContact();
 			}
+		}
+		System.out.print("Please enter your SSN");
+		String ssng = obj.nextLine();
+		int ssn = Integer.parseInt(ssng);
 		if (status == "C") {
 			System.out.print("Please enter your email");
 			String email = obj.nextLine();
 			System.out.print("Please enter your phone number");
 			String phonenumber = obj.nextLine();
-			System.out.print("Please enter your SSN");
-			String ssng = obj.nextLine();
-			int ssn = Integer.parseInt(ssng);
 			System.out.print("Residence Region?");
 			String region = obj.nextLine();
-			CovidCases patient = new CovidCases();
-			patient.email = email;
-			patient.patientPhone = phonenumber;
-			patient.ssn = ssn;
+				if (region = null ) {
+					CovidCases patient = new CovidCases(region, ssn);
+				}else {
+					CovidCases patient = new CovidCases(ssn);
+				}
 			//stelnoume password
 		}else if (status == "P") {
 			System.out.print("Please enter your verification code");
@@ -48,30 +50,12 @@ public class Corona{
 			String phonenumber = obj.nextLine();
 			System.out.print("Please enter your email");
 			String email = obj.nextLine();
-			
-			User us = new User();
-			us.password = ver_code;
-			us.username = name;
-			us.symptoms = new int[13];			
+			if (ssn = null ) {
+				User us = new User(ver_code, name, name);
+			}else {
+				User us = new User(ver_code, name, ssn, name);
+			}	
 			//stelnoume password
-		}
-		//date eggrafhs
-		LocalDateTime signupdate = LocalDateTime.now();  
-		//katagrafh symptwmatwn
-		symptomsListing();
-		// katagrafh close contacts
-		System.out.print("Close contact can mean:" +
-				" 1) spending more than 15 minutes of face-to-face contact" +
-				"within 2 metres of someone who has COVID-19, indoors or outdoors" +
-				" 2) living in the same house or shared accommodation as someone who has COVID-19" +
-				" 3) sitting within 2 seats of someone who has COVID-19 on public transport or an airplane");
-		System.out.print("How many close contacts do you have");
-		String ccg = obj.nextLine();
-		int ccnum = Integer.parseInt(ccg);
-		if ccnum != 0 {
-			for (int i = 0; i < ccnum; i++) {
-				addContact();
-			}
 		}
 	}
 }
