fe6f13ccdee9d7c18b3f52a2e088e7ffb9bbc6f9
Iro <72868429+iroharakopoulou@users.noreply.github.com>
2020-12-22 11:57:11 +0200

    προσθήκη του sign up

--- src/project_spin/Corona.java
+++ src/project_spin/Corona.java
@@ -2,6 +2,7 @@ package project_spin;
 
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.time.LocalDateTime;
 
 final public class Corona {
 	public static void main(Strings[] args) {
@@ -30,8 +31,59 @@ final public class Corona {
 			System.err.println("This mail or phone does not exist");
 			System.err.println("Please try again");
 		}
-
-
+		// if you want to sing up
+		Scanner obj= new Scanner(System.in);
+		System.out.print("Please enter your name");
+		String name = obj.nextLine();
+		System.out.print("Please enter your status:C for covid patient for P proti epafi");
+		String status = obj.nextLine();
+		//date eggrafhs
+		LocalDateTime signupdate = LocalDateTime.now();  
+		//katagrafh symptwmatwn
+		symptomsListing();
+		// katagrafh close contacts
+		System.out.print("Close contact can mean:" +
+				" 1) spending more than 15 minutes of face-to-face contact" +
+				"within 2 metres of someone who has COVID-19, indoors or outdoors" +
+				" 2) living in the same house or shared accommodation as someone who has COVID-19" +
+				" 3) sitting within 2 seats of someone who has COVID-19 on public transport or an airplane");
+		System.out.print("How many close contacts do you have");
+		String ccg = obj.nextLine();
+		int ccnum = Integer.parseInt(ccg);
+		if ccnum != 0 {
+			for (int i = 0; i < ccnum; i++) {
+				addContact();
+			}
+		}
+		System.out.print("Please enter your SSN");
+		String ssng = obj.nextLine();
+		int ssn = Integer.parseInt(ssng);
+		if (status == "C") {
+			System.out.print("Please enter your email");
+			String email = obj.nextLine();
+			System.out.print("Please enter your phone number");
+			String phonenumber = obj.nextLine();
+			System.out.print("Residence Region?");
+			String region = obj.nextLine();
+				if (region = null ) {
+					CovidCases patient = new CovidCases(region, ssn);
+				}else {
+					CovidCases patient = new CovidCases(ssn);
+				}
+			//stelnoume password
+		} else if (status == "P") {
+			System.out.print("Please enter your verification code");
+			String ver_code = obj.nextLine();
+			System.out.print("Please enter your phone number");
+			String phonenumber = obj.nextLine();
+			System.out.print("Please enter your email");
+			String email = obj.nextLine();
+			if (ssn = null ) {
+				User us = new User(ver_code, name, name);
+			}else {
+				User us = new User(ver_code, name, ssn, name);
+			}	
+			//stelnoume password
 	}
 }
 	
