5b3e8ad18754413e0477350f6dede2f4fbd890c1
Ioanna <73000452+Ioanna2001@users.noreply.github.com>
2021-01-17 21:54:20 +0200

    Revert "Revert "Η αρχή των γραφικών!""
    This reverts commit 216c863aa91536b7f7515164485aa8263417c0b1.

--- src/project_spin/Corona.java
+++ src/project_spin/Corona.java
@@ -1,32 +1,35 @@
 package project_spin;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.util.Scanner;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseListener;
+import java.awt.event.ActionEvent;
 
-public class Corona {
+public final class Corona {
 	static Scanner obg = new Scanner(System.in);
+	static String status = null;
+	static String email;
+	static String name;
+	static String password;
 	public static void main(String[] args) throws Exception {
 		if (Ssn.i == 0) {
 			Ssn.ssnInitiator();
 		}
+		GUI.gui();
 		
-		Scanner in = new Scanner(System.in);
-		System.out.println("Welcome to the EODY Platform Covid Tracker");
-		System.out.println("If you want to log in press 1\nIf you want to sign up press 2");
-		System.out.print("Selection: ");
-		switch (in.nextInt()) 
-		{
-		case 1: //log in
-		login();
-		break;
-		case 2: // sing up
-		signup();
-		break;
-		default:
-		System.err.println("Unrecognized option");
-		return;
-		}
 		obg.close();
-		in.close();
 	}
 	// if you want to log in
 	public static void login() {
@@ -80,54 +83,7 @@ public class Corona {
 
 	// if you want to sing up
 	public static void signup() throws Exception {
-		Scanner obg = new Scanner(System.in);
-		String name = null;
-		//an to name einai null petaei exception
-		try {
-			System.out.println("Please enter your name:");
-			name = obg.nextLine();
-			if (name == null) {
-				throw new NullPointerException();
-			}
-		} catch (NullPointerException e) {
-			System.out.println("Please make sure the name field is not empty");
-			while (name ==  null) {
-				System.out.println("Please enter your name:");
-				name = obg.nextLine();
-			}
-		}
-		//an to status einai null h invalid petaei error
-		String status = null;
-		try {
-			System.out.println("Please enter your status:/nC for covid patient/nF for first contact:");
-			status = obg.nextLine();
-			if (status == null) {
-				throw new NullPointerException();
-			}
-			while (!(status.equals("C") || (status.equals("F")))) {
-				System.out.println("Invalid answer.");
-				System.out.println("Please enter your status:/nC for covid patient/nF for first contact*");
-				status = obg.nextLine();
-				if (status == null) {
-					throw new NullPointerException();
-				}
-			}
-		} catch (NullPointerException e) {
-			System.out.print("Please make sure the name field is not empty");
-			while (name ==  null) {
-				System.out.println("Please enter your status:/nC for covid patient/nF for first contact*");
-				status = obg.nextLine();
-				while (!(status.equals("C") || !(status.equals("F")))) {
-					System.out.println("Invalid answer.");
-					System.out.println("Please enter your status:/nC for covid patient/nF for first contact*");
-					status = obg.nextLine();
-					if (status == null) {
-						throw new NullPointerException();
-					}
-				}
-			}
-		} 
-
+		GUI.gui3();
 		//sing up covid case user
 		if (status.equals("C")) {
 			//CovidCase object
@@ -139,8 +95,7 @@ public class Corona {
 	}
 
 	private static void firstContact(String name) throws Exception {
-		String password = passwordCheck();
-		String email = emailCheck();
+
 		User firstContact = new User(password, name, email);
 		//eisagwgh prwtwn epafwn
 		contacts(firstContact);
@@ -162,27 +117,7 @@ public class Corona {
 		user.editContactList();
 	}
 
-	private static String emailCheck() {
-		String email;
-		try {
-			System.out.print("Please enter your email (example: user@test.com)*");
-			email = obg.nextLine(); 
-			//an to mail den einai valid petaei exception
-			if (SendEmail.isValid(email) == false) {
-				throw new WrongAnswerException();
-			}
-		} catch (Exception exce) {
-			System.out.print("Please make sure you entered a valid email. Try again ");
-			do {
-				System.out.println("Please enter your email (example: user@test.com)*");
-				email = obg.nextLine();
-			} while (SendEmail.isValid(email) == false);
-		}
-		return email;
-	}
-
-	private static String passwordCheck() {
-		String password;
+	private static void passwordCheck() {
 		System.out.println("Please enter your verification code:");
 		password = obg.nextLine();
 		int tries = 1;//the user can enter the code 5 times
@@ -195,7 +130,6 @@ public class Corona {
 			System.out.println("Invalid code.");
 			System.exit(0);
 		}
-		return password;
 	}
 
 	protected static int checkAge() {
@@ -253,12 +187,11 @@ public class Corona {
 	private static void covidCase(String name) throws Exception {
 		int ssn = checkSsn();
 		//o xrhsths eisagei to email
-		String email = emailCheck();
 		//stelnetai to mail ston xrhsth
 		SendEmail sendEmail = new SendEmail(email);
 		sendEmail.covidCaseMail();
 		//afoy stalei o xrhsths bazei to verification code
-		String password = passwordCheck();
+		
 		//Eisagwgh perioxhs
 		Location location = checkLocation();
 		//eisagwgh age
@@ -279,6 +212,7 @@ public class Corona {
 		patient.addSymptoms();
 		InputMenu.question(patient);
 	}
+
 }
 
 
